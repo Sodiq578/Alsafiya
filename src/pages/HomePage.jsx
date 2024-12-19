1d3e7f7356ef389c45717a6d874a46195fc76307
Sodiq578 <sodiqjon241@gmail.com>
2024-12-19 18:05:44 +0500

    modla qo'shildi

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,10 +6,7 @@ import HeaderImg from "../assets/headeimg2.png";
 import CardImage2 from "../assets/kist-ul-dori.jpg";
 import cardImage3 from "../assets/black_sid-oil.jpg";
 import { AiOutlineEye } from "react-icons/ai";
-import Logo from "../assets/logo2.svg"
-
-
-// Import React Icons at the top
+import Logo from "../assets/logo2.svg";
 import { FaBuilding, FaBox, FaUsers, FaHandshake } from "react-icons/fa";
 
 const HomePage = () => {
@@ -18,70 +15,24 @@ const HomePage = () => {
   const [selectedImage, setSelectedImage] = useState(null);
   const [animated, setAnimated] = useState(false);
 
+  const [isUserModalOpen, setUserModalOpen] = useState(true); // Modal state
+  const [userData, setUserData] = useState({ name: "", phone: "" }); // User data
 
-  
+  const token = "7747931873:AAEx8TM-ddgYOQtnr6cyGGnT1nzC7ElG4u0"; // Telegram bot token
+  const groupChatId = "-1002480723282"; // Telegram group chat ID
+
   const [counters, setCounters] = useState([
-    {
-      id: 1,
-      icon: <FaBuilding size={60} color="#19524B" />,
-      title: "Filiallar",
-      value: 0,
-      target: 4,
-    },
-    {
-      id: 2,
-      icon: <FaBox size={60} color="#19524B" />,
-      title: "Mahsulotlar",
-      value: 0,
-      target: 8000,
-    },
-    {
-      id: 3,
-      icon: <FaUsers size={60} color="#19524B" />,
-      title: "Mijozlarimiz",
-      value: 0,
-      target: 7420,
-    },
-    {
-      id: 4,
-      icon: <FaHandshake size={60} color="#19524B" />,
-      title: "Hamkorlar",
-      value: 0,
-      target: 23,
-    },
+    { id: 1, icon: <FaBuilding size={60} color="#19524B" />, title: "Filiallar", value: 0, target: 4 },
+    { id: 2, icon: <FaBox size={60} color="#19524B" />, title: "Mahsulotlar", value: 0, target: 8000 },
+    { id: 3, icon: <FaUsers size={60} color="#19524B" />, title: "Mijozlarimiz", value: 0, target: 7420 },
+    { id: 4, icon: <FaHandshake size={60} color="#19524B" />, title: "Hamkorlar", value: 0, target: 23 },
   ]);
 
   const cards = [
-    {
-      id: 1,
-      title: "Kist-ul Hindi",
-      price: 400000,
-      image: CardImage2,
-      description: "Tibbio Tibomed kompaniyasi sog‘liqni saqlash sohasida o‘zining yuqori sifatli biologik faol qo‘shimchalari bilan tanilgan. Ushbu kompaniya tomonidan ishlab chiqarilgan Omega-3 kapsulalari – sog‘lom turmush tarzini qo‘llab-quvvatlashga qaratilgan mukammal mahsulotdir. Omega-3 ning ilmiy asoslangan foydalari va Tibbio Tibomed kompaniyasining mukammallikka intilishi bu mahsulotni sog‘lom hayot uchun ajralmas tanlovga aylantiradi.",
-    },
-
-    {
-      id: 2,
-      title: "Omega-3",
-      price: 399000,
-      image: CardImage,
-      description: "Miya faoliyati: Xotira va konsentratsiyani yaxshilaydi, stressni kamaytirishga yordam beradi.Ko'z sog'ligi: Ko'rishni mustahkamlaydi, ko‘z charchoqlarini kamaytiradi. Immunitet: Tabiiy himoya tizimini kuchaytirib, kasalliklarga qarshi kurashishda yordam beradi. Teri va soch: Teri elastikligini oshirib, yosh va sog‘lom ko‘rinishni saqlaydi; sochlarni mustahkam va yorqin qiladi.",
-    },
-
-    {
-      id: 3,
-      title: "Qora sedana",
-      price: 399000 ,
-      image: cardImage3,
-      description: "Qora sedana – qadimiy davolovchi o‘simlik bo‘lib, uning urug‘lari ko‘plab xalqlarning an’anaviy tabobatlarida keng qo‘llanib kelinadi. U asosan Janubi-G‘arbiy Osiyo, Yaqin Sharq, Hindiston va Shimoliy Afrika hududlarida o‘sadi. Qur'oni Karimda ham qora sedana (Habba Sauda) haqida zikr qilingan va bu o‘simlikning shifobaxsh xususiyatlari Payg‘ambarimiz Muhammad (s.a.v.) hadislarida maqtov bilan tilga olingan.",
-    },
-    {
-      id: 4,
-      title: "Kist-ul Hindi",
-      price: 400000,
-      image: CardImage2,
-      description: "Tibbio Tibomed kompaniyasi sog‘liqni saqlash sohasida o‘zining yuqori sifatli biologik faol qo‘shimchalari bilan tanilgan. Ushbu kompaniya tomonidan ishlab chiqarilgan Omega-3 kapsulalari – sog‘lom turmush tarzini qo‘llab-quvvatlashga qaratilgan mukammal mahsulotdir. Omega-3 ning ilmiy asoslangan foydalari va Tibbio Tibomed kompaniyasining mukammallikka intilishi bu mahsulotni sog‘lom hayot uchun ajralmas tanlovga aylantiradi.",
-    },
+    { id: 1, title: "Kist-ul Hindi", price: 400000, image: CardImage2, description: "Tibbio Tibomed kompaniyasi sog‘liqni saqlash sohasida..." },
+    { id: 2, title: "Omega-3", price: 399000, image: CardImage, description: "Miya faoliyati: Xotira va konsentratsiyani yaxshilaydi..." },
+    { id: 3, title: "Qora sedana", price: 399000, image: cardImage3, description: "Qora sedana – qadimiy davolovchi o‘simlik..." },
+    { id: 4, title: "Kist-ul Hindi", price: 400000, image: CardImage2, description: "Tibbio Tibomed kompaniyasi sog‘liqni saqlash sohasida..." },
   ];
 
   useEffect(() => {
@@ -120,9 +71,7 @@ const HomePage = () => {
 
   const updateCounter = (index, value) => {
     setCounters((prev) =>
-      prev.map((counter, i) =>
-        i === index ? { ...counter, value: value } : counter
-      )
+      prev.map((counter, i) => (i === index ? { ...counter, value: value } : counter))
     );
   };
 
@@ -139,45 +88,98 @@ const HomePage = () => {
     setImageModalOpen(false);
     setSelectedImage(null);
   };
+
+  const handlePhoneChange = (e) => {
+    let value = e.target.value;
+
+    // Telefon raqamini formatlash
+    if (!value.startsWith("+998")) {
+      value = "+998" + value.replace(/[^0-9]/g, "").slice(0, 10); // Faqat raqamlarni qabul qilish va 9 raqamga cheklash
+    } else {
+      value = "+998" + value.slice(4).replace(/[^0-9]/g, "").slice(0, 10); // Faol raqamlarni to'g'ri formatda saqlash
+    }
+
+    if (value.length <= 13) {
+      setUserData((prev) => ({ ...prev, phone: value }));
+    }
+  };
+
+  const [error, setError] = useState(null); // Xatolik holati
+
+  const handleUserSubmit = () => {
+    if (userData.name && userData.phone) {
+      const message = `
+      
+      Yangi foydalanuchi saytdna ro'yxatdan o'tdi
+
+      👤 Ismi: ${userData.name}\n📞 Telefon: ${userData.phone}`;
+      fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ chat_id: groupChatId, text: message }),
+      })
+        .then(() => {
+          setUserModalOpen(false);
+          setUserData({ name: "", phone: "" }); // Formani tozalash
+          setError(null); // Xatolikni tozalash
+        })
+ 
+    }
+  };
+  
+
   return (
     <div className="home-page">
+      {isUserModalOpen && (
+        <div className="user-modal">
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <h2>Ro'yxatdan o'tish</h2>
+            <input
+              type="text"
+              placeholder="Ismingizni kiriting"
+              value={userData.name}
+              onChange={(e) => setUserData((prev) => ({ ...prev, name: e.target.value }))} />
+            <input
+              type="tel"
+              placeholder="Telefon raqamingizni kiriting"
+              value={userData.phone}
+              onChange={handlePhoneChange}
+              maxLength="13" // Telefon raqami uchun maksimal uzunlik
+            />
+            <button onClick={handleUserSubmit}>Yuborish</button>
+          </div>
+        </div>
+      )}
       <div className="header-box">
         <img className="header-img" src={HeaderImg} alt="Header" />
         <img className="logo-img" src={Logo} alt="Header Logo" />
       </div>
 
-      <h1 className="title">Top Maxsulotlar</h1>
+      <h1 className="title">Top Mahsulotlar</h1>
 
       <div className="container card-container">
-  {cards.map((card) => (
-    <div className="card" key={card.id} onClick={() => handleCardClick(card)}>
-      <div className="card-image-wrapper">
-        <img
-          src={card.image}
-          alt={card.title}
-          className="card-image"
-        />
-        <div className="overlay-icon" onClick={() => openImageModal(card.image)}>
-          <AiOutlineEye size={20} />
-        </div>
+        {cards.map((card) => (
+          <div className="card" key={card.id} onClick={() => handleCardClick(card)}>
+            <div className="card-image-wrapper">
+              <img src={card.image} alt={card.title} className="card-image" />
+              <div className="overlay-icon" onClick={() => openImageModal(card.image)}>
+                <AiOutlineEye size={20} />
+              </div>
+            </div>
+            <div className="card-details">
+              <h3 className="card-title">{card.title}</h3>
+            </div>
+          </div>
+        ))}
       </div>
-      <div className="card-details">
-        <h3 className="card-title">{card.title}</h3>
-      </div>
-    </div>
-  ))}
-</div>
 
-
+      {/* Statistikalar */}
       <div className="count-area">
         <div className="container">
           <h2 className="title">Statistika</h2>
           <div className="row">
             {counters.map((counter) => (
-              <div
-                className={`col-md-3 count-card ${animated ? "animate" : ""}`}
-                key={counter.id}
-              >
+              <div className={`col-md-3 count-card ${animated ? "animate" : ""}`} key={counter.id}>
                 <div className="count-area-content">
                   <div className="count-icon">{counter.icon}</div>
                   <div className="count-digit">{counter.value}</div>
